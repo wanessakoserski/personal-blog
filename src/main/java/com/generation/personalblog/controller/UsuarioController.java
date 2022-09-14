b0efa032759cd387f7f00905882c0bc885fc92bf
Wanessa Koserski <wkoserski@gmail.com>
2022-09-14 14:12:07 -0300

    :sparkles: feat: endpoint to delete user

--- src/main/java/com/generation/personalblog/controller/UsuarioController.java
+++ src/main/java/com/generation/personalblog/controller/UsuarioController.java
@@ -9,9 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,6 +57,11 @@ public class UsuarioController {
 		return userService.update(usuario)
 			.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
 			.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		userRepository.deleteById(id);
 	}
 	
 }
